509a5ef8788229c6105c863580d9a187a9ed1121
ke2xin <2724943481@qq.com>
2023-05-17 14:41:00 +0800

    添加controller生产消息到消费者

--- src/main/java/com/example/springkafka/controller/HelloWord.java
+++ src/main/java/com/example/springkafka/controller/HelloWord.java
@@ -1,12 +1,22 @@
 package com.example.springkafka.controller;
 
+import com.example.springkafka.provider.KafkaSender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWord {
+    @Autowired
+    private KafkaSender sender;
     @RequestMapping("/")
     public String index(){
         return  "首页";
+    }
+
+    @RequestMapping("/send")
+    public String send(){
+        sender.send();
+        return  "send";
     }
 }
